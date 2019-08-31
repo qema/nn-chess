d9fbd79bb0d551d7cef39cd325cc5674f3e8aee9
Andrew Wang <azw7@cornell.edu>
2019-08-31 16:12:48 -0700

    initial commit

---
+++ common.py
@@ -0,0 +1,122 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import chess
+import random
+
+class PolicyModel(nn.Module):
+    def __init__(self):
+        super(PolicyModel, self).__init__()
+        self.conv1 = nn.Conv2d(36, 128, 5, padding=2)
+        self.relu1 = nn.ReLU()
+        self.conv2 = nn.Conv2d(128, 128, 5, padding=2)
+        self.relu2 = nn.ReLU()
+        self.conv3 = nn.Conv2d(128, 128, 3, padding=1)
+        self.relu3 = nn.ReLU()
+        #self.conv4 = nn.Conv2d(128, 128, 3, padding=1)
+        #self.relu4 = nn.ReLU()
+        self.fc1 = nn.Linear(64*128, 256)
+        self.relu4 = nn.ReLU()
+        self.fc2 = nn.Linear(256, 64*64)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, boards):
+        out = self.conv1(boards)
+        out = self.relu1(out)
+        out = self.conv2(out)
+        out = self.relu2(out)
+        out = self.conv3(out)
+        out = self.relu3(out)
+        #out = self.conv4(out)
+        out = out.view(out.shape[0], -1)
+        out = self.fc1(out)
+        out = self.relu4(out)
+        out = self.fc2(out)
+        out = self.softmax(out)
+        #out = out.view(out.shape[0], 2, 8, 8)
+        return out
+
+device_cache = None
+def get_device():
+    global device_cache
+    if device_cache is None:
+        device_cache = torch.device("cuda") if torch.cuda.is_available() \
+            else torch.device("cpu")
+    return device_cache
+
+# input: list of fens
+def states_to_tensor(states):
+    boards_t = []
+    for state in states:
+        board = chess.Board(state)
+        side = board.turn
+        piece_map = board.piece_map()
+
+        pieces_t = torch.zeros(12, 8, 8, device=get_device())
+        for pos, piece in piece_map.items():
+            col, row = chess.square_file(pos), chess.square_rank(pos)
+            idx = int(piece.piece_type != side)*6 + (piece.piece_type-1)
+            pieces_t[idx][row][col] = 1
+
+        legal_t = torch.zeros(12, 8, 8, device=get_device())
+        capture_t = torch.zeros(12, 8, 8, device=get_device())
+        for move in board.legal_moves:
+            piece = piece_map[move.from_square]
+            to_pos = move.to_square
+            col, row = chess.square_file(to_pos), chess.square_rank(to_pos)
+            idx = int(piece.piece_type != side)*6 + (piece.piece_type-1)
+            legal_t[idx][row][col] = 1
+            if to_pos in piece_map:
+                capture_t[idx][row][col] = 1
+
+        board_t = torch.cat((pieces_t, legal_t, capture_t), dim=0)
+        boards_t.append(board_t)
+    boards_t = torch.stack(boards_t)
+    return boards_t
+
+def action_idx_to_move(idx):
+    return chess.Move(idx // 64, idx % 64)
+
+def move_to_action_idx(move):
+    return move.from_square*64 + move.to_square
+
+# input: list of uci moves
+def moves_to_tensor(moves):
+    action_tensors = []
+    for t, action in enumerate(moves):
+        move = chess.Move.from_uci(action)
+        action_tensors.append(move_to_action_idx(move))
+    action_tensors = torch.tensor(action_tensors, dtype=torch.long,
+        device=get_device())
+    return action_tensors
+
+def choose_move(board, model, eps):
+    legal_moves = list(board.legal_moves)
+    if random.random() < eps:
+        move = random.choice(legal_moves)
+    else:
+        board_t = states_to_tensor([board.fen()])
+        pred = model(board_t)
+
+        valid_idxs = [move_to_action_idx(move) for move in legal_moves]
+        pred = pred[0][valid_idxs]
+        actions = torch.distributions.Categorical(logits=pred)
+        move = legal_moves[actions.sample().item()]
+        if move.promotion is not None:
+            move.promotion = 5
+    return move
+
+reward_dict = {"1-0": 1, "0-1": -1, "1/2-1/2": 0}
+# precond: game is over
+def reward_for_side(board, side):
+    result = board.result()
+    reward = reward_dict[result]
+    if not side:
+        reward *= -1
+    return reward
+
+if __name__ == "__main__":
+    policy = PolicyModel()
+    board = chess.Board()
+    boards_t = states_to_tensor([board.fen()])
+    policy(boards_t)
---
+++ reinforce.py
@@ -0,0 +1,105 @@
+from common import *
+import random
+import queue
+
+game_batch_size = 16#128
+max_recent_opps = 10000
+pool_update_dur = 2#64
+
+def train(model, opt, criterion, boards, actions, rewards):
+    model.zero_grad()
+    pred = model(boards)
+    loss = criterion(pred, actions)
+    loss *= rewards
+    loss = torch.sum(loss) / game_batch_size
+    loss.backward()
+    opt.step()
+    return loss
+
+def run_games(n_games, queue, model, opp_model, epoch):
+    moves = [[] for i in range(n_games)]
+    states = [[] for i in range(n_games)]
+    rewards = [[] for i in range(n_games)]
+    boards = [chess.Board() for i in range(n_games)]
+    n_done = 0
+    done_idxs = set()
+    t = 0
+
+    while n_done < n_games:
+        board_t = states_to_tensor([board.fen() for board in boards])
+        if t % 2 == 0:
+            pred_l = model(board_t[:n_games//2])
+            pred_r = opp_model(board_t[n_games//2:])
+        else:
+            pred_l = opp_model(board_t[:n_games//2])
+            pred_r = model(board_t[n_games//2:])
+        pred = torch.cat((pred_l, pred_r), dim=0)
+
+        for n, board in enumerate(boards):
+            if not board.is_game_over():
+                legal_moves = list(board.legal_moves)
+                valid_idxs = [move_to_action_idx(move) for move in legal_moves]
+                pred_n = pred[n][valid_idxs]
+                actions = torch.distributions.Categorical(logits=pred_n)
+                move = legal_moves[actions.sample().item()]
+                if move.promotion is not None:
+                    move.promotion = 5
+                if (n < n_games//2) == (t % 2 == 0):
+                    moves[n].append(move.uci())
+                    states[n].append(board.fen())
+                board.push(move)
+            else:
+                if n not in done_idxs:
+                    done_idxs.add(n)
+                    n_done += 1
+                    reward = reward_for_side(board, n < n_games//2)
+                    #print(n, board.result(), reward)
+                    rewards[n] += [reward]*len(moves[n])
+
+                    queue.put((moves[n], states[n], rewards[n]))
+        t += 1
+
+
+if __name__ == "__main__":
+    model = PolicyModel().to(get_device())
+    opp_model = PolicyModel().to(get_device())
+    #opp_model.load_state_dict(torch.load("models/supervised.pt",
+    #    map_location=get_device()))
+    opp_model_pool = []
+
+    opt = optim.Adam(model.parameters(), lr=1e-3)
+    #opt = optim.SGD(model.parameters(), lr=1e-5)
+    criterion = nn.NLLLoss(reduction="none")
+
+    for epoch in range(10000):
+        print("Epoch {}".format(epoch))
+        # play n games
+        moves, states, rewards = [], [], []
+        with torch.no_grad():
+            q = queue.Queue()
+            run_games(game_batch_size, q, model, opp_model, epoch)
+
+            while not q.empty():
+                m, s, r = q.get()
+                moves += m
+                states += s
+                rewards += r
+
+        # train
+        boards = states_to_tensor(states)
+        actions = moves_to_tensor(moves)
+        rewards = torch.tensor(rewards, dtype=torch.float,
+            device=get_device())
+        loss = train(model, opt, criterion, boards, actions, rewards)
+        print("Loss: {:.6f}".format(loss.item()))
+        print()
+
+        torch.save(model.state_dict(), "models/reinforce.pt")
+
+        if epoch % pool_update_dur == 0:
+            opp_model_pool.append(model.state_dict())
+            opp_model_pool = opp_model_pool[-max_recent_opps:]
+
+        # pick random opponent out of pool
+        params = random.choice(opp_model_pool)
+        opp_model.load_state_dict(params)
